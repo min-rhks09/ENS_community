61172b6a179a9f664d8478aaf6a67129a32408f5
min_rhks09 <220805min@gmail.com>
2023-07-11 15:52:43 +0900

    0711

--- enscommunity/src/pages/connectwallet/connectwallet.js
+++ enscommunity/src/pages/connectwallet/connectwallet.js
@@ -20,125 +20,139 @@ let Pagetitle = styled.div`
   font-weight: 500;
 `
 
-<<<<<<< HEAD
 let Button = styled.button`
   background-color: #D6EF0E;
   text-align: center;
   display: inline-block;
 
-=======
-
-
-let Icon = styled.img`
-  width: ${ props => props.wi };
-  height:${ props => props.he };
-  margin: auto;
-  margin-top:2vw;
-  display: flex;
-  justify-content: center;
 `
 
-let P = styled.p`
-  font-size: 20px;
-`
-
-let List =styled.div`
-  font-size: 20px;
-`
-
-let Buttongroup =styled.div`
-  width: 100%;
-  text-align: center;
-  display:inline-block;
->>>>>>> e7bb727f92e489c204afb1e7dd0f91df7fcb9632
-`
-
-function Connectwallet() {
-
-  const metamaskwebsite =()=>{
-    window.open('https://metamask.io/');
-  };
-
-  const downloadmetamask = () => {
-    window.open('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn');
-  };
-
-  const [provider, setProvider] = useState(undefined);
-  const [signer, setSigner] = useState(undefined)
-  const [walletAddress, setWalletAddress] = useState(undefined)
-  const [currentBalance, setCurrentBalance] = useState(undefined)
-  const [chainId, setChainId] = useState(undefined)
-  const [isConnected,setIsConnected] = useState(false)
-
-  const connectwallet = useCallback(async () => {
-    try {
-      if(typeof window.ethereum !== 'undefined') {
-        await getMetamaskData();
-
-        setIsConnected(true);
-      } else {
-        alert("please install MetaMask")
+  function Connectwallet() {
+    const downloadmetamask = () => {
+      window.open('https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn');
+    };
+  
+    const [provider, setProvider] = useState(undefined);
+    const [signer, setSigner] = useState(undefined)
+    const [walletAddress, setWalletAddress] = useState(undefined)
+    const [currentBalance, setCurrentBalance] = useState(undefined)
+    const [chainId, setChainId] = useState(undefined)
+    const [isConnected,setIsConnected] = useState(false)
+  
+    const openmetamask = useCallback(async () => {
+      
+      const chainId = 777
+  
+      if (window.ethereum.networkVersion !== chainId) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_switchEthereumChain',
+            params: [{ chainId: web3.utils.toHex(chainId) }]
+          });
+        } catch (err) {
+            // This error code indicates that the chain has not been added to MetaMask
+          if (err.code === 4902) {
+            await window.ethereum.request({
+              method: 'wallet_addEthereumChain',
+              params: [
+                {
+                  chainName: 'Polygon Mainnet',
+                  chainId: web3.utils.toHex(chainId),
+                  nativeCurrency: { name: 'MATIC', decimals: 18, symbol: 'MATIC' },
+                  rpcUrls: ['http://172.30.1.72:8545']
+                }
+              ]
+            });
+          }
+        }
       }
-    } catch (error) {
-      console.log(error);
+  
+      try {
+        if(typeof window.ethereum !== 'undefined') {
+          await getMetamaskData();
+  
+          setIsConnected(true);
+        } else {
+          alert("please install MetaMask")
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },[])
+  
+    const getMetamaskData = async () => {
+      const _provider = await getProvider();
+      const _signer = await getSigner(_provider);
+      await getWalletData(_signer);
     }
-  },[])
-
-
-  const getMetamaskData = async () => {
-    const _provider = await getProvider();
-    const _signer = await getSigner(_provider);
-    await getWalletData(_signer);
-  }
-
-  const getProvider = async () => {
-    const provider = await new ethers.providers.Web3Provider(window.ethereum);
-    setProvider(provider);
-
-    return provider;
-  }
-
-  const getSigner = async (provider) => {
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    setSigner(signer) 
-
-    return signer;
-  }
   
-  const getWalletData = async(signer) => {
-    const result = await Promise.all([signer.getAddress(),signer.getBalance(),signer.getChainId()])
-    setWalletAddress(result[0])
-    setCurrentBalance(Number(ethers.utils.formatEther(result[1])))
-    setChainId(result[2])
+    const getProvider = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
   
-  }
-  return (
-  <Connectwalletst>
-    <Pagetitle>
-      CONNECT WALLET
-    </Pagetitle>
-    <hr/>
-    <Icon wi='80%' he='70%' src={icon} />
-    <Buttongroup>
-    <Button onClick={metamaskwebsite}>
-      GO WEBSITE
-    </Button>
-    <Button onClick={downloadmetamask}>
-    DOWNLOAD METAMASK
-    </Button>
-    <Button onClick={openmetamask}>
-    <ConnectButton>
-              isConnected={isConnected}
-              connectWallet={openmetamask}
-              walletAddress={walletAddress}
-              currentBalance={currentBalance}
-              chainId={chainId}
->>>>>>> e7bb727f92e489c204afb1e7dd0f91df7fcb9632
-    </ConnectButton>
-
-    </Buttongroup>
-
+      return provider;
+    }
+  
+    const getSigner = async (provider) => {
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      setSigner(signer) 
+  
+      return signer;
+    }
+    
+    const getWalletData = async(signer) => {
+      const result = await Promise.all([signer.getAddress(),signer.getBalance(),signer.getChainId()])
+      setWalletAddress(result[0])
+      setCurrentBalance(Number(ethers.utils.formatEther(result[1])))
+      setChainId(result[2])
+      console.log(result)
+  
+      let data = {
+        walletAddress: result
+      };
+      
+      fetch('http://172.30.1.14:3000', {
+        method: "post",
+        headers :{
+          "Content-Type" : "application/json; charset=utf-8"
+        },
+        body: JSON.stringify(data)
+      }).then(res => res.json())
+        .then(result => {
+          console.log(result);
+        });
+      
+  
+    }
+    return (
+    <Connectwalletst>
+      <Pagetitle>
+        CONNECT WALLET
+      </Pagetitle>
+      <hr/>
+      <Button onClick={downloadmetamask}>
+        download Metamask
+      </Button>
+  
+      <Button onClick={openmetamask}>
+      <ConnectButton>
+                isConnected={isConnected}
+                connectWallet={openmetamask}
+                walletAddress={walletAddress}
+                currentBalance={currentBalance}
+                chainId={chainId}
+      </ConnectButton>
+      </Button>
+      {isConnected && walletAddress && (
+          <div>
+            <h2>Wallet Address</h2>
+            <p>{walletAddress}</p>
+  
+            <h2>Current Balance</h2>
+            <p>{currentBalance} ETH</p>
+          </div>
+        )}
   <h5>MetaMask 이용하기</h5>
   <P>우리 서비스를 이용하기위해서는 메타마스크를 이용해야 합니다</P>
   <h6>step1: MetaMask 다운로드</h6>
